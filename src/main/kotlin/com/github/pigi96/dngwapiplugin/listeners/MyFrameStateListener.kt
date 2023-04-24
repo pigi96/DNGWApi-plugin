eb8023dec4d140f1ceb61569aa9953b21cc9a563
GitHub Action <action@github.com>
2023-04-24 12:38:35 +0000

    Template cleanup

--- src/main/kotlin/com/github/pigi96/dngwapiplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/pigi96/dngwapiplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.pigi96.dngwapiplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
